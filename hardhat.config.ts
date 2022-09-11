996ff9412f04e5c4adbded7cd3902cbbb074b3b4
Shunchao Wang <shunchao.wang@kohls.com>
2022-09-11 19:55:05 -0400

    feat:add vrf mocking service

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,6 +18,9 @@ const config: HardhatUserConfig = {
             {
                 version: "0.8.9",
             },
+            {
+                version: "0.8.0",
+            },
         ],
     },
     defaultNetwork: "hardhat",
--- scripts/00-deploy-mocks.ts
+++ scripts/00-deploy-mocks.ts
@@ -37,3 +37,79 @@ const deployMocks: DeployFunction = async function (
 }
 export default deployMocks
 deployMocks.tags = ["all", "mocks", "main"]
+
+// since we are using vrf chainlink call, we need to mock the vrf endpoint
+import { ethers, network } from "hardhat"
+import {
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+} from "../helper-hardhat-config"
+import verify from "../utils/verify"
+
+import * as dotenv from "dotenv"
+
+dotenv.config()
+
+async function main() {
+    const [deployer] = await ethers.getSigners()
+    console.log(`Deploying contracts with the account ${deployer.address}`)
+    console.log(`Account balance: ${(await deployer.getBalance()).toString()}`)
+
+    // deploy vrf coordinator
+    // deploy aggregator
+
+    // we only want to have 1 confirmation if on localhost and hardhat dev node
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
+
+    console.log()
+
+    // If we are on a local development network, we need to deploy mocks!
+    const chainId = network.config.chainId
+    if (chainId == 31337) {
+        log("Local network detected! Deploying mocks...")
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: [BASE_FEE, GAS_PRICE_LINK],
+        })
+        await deploy("MockV3Aggregator", {
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_PRICE],
+        })
+
+        log("Mocks Deployed!")
+        log("----------------------------------")
+        log("You are deploying to a local network, you'll need a local network running to interact")
+        log(
+            "Please run `yarn hardhat console --network localhost` to interact with the deployed smart contracts!"
+        )
+        log("----------------------------------")
+
+    const basicNftContract = await ethers.getContractFactory("BasicNft")
+    const nft = await basicNftContract.deploy()
+
+    await nft.deployTransaction.wait(waitBlockConfirmations)
+    console.log(`Waiting for the block confirmation ${waitBlockConfirmations}`)
+
+    console.log(`Nft deployed at ${nft.address}`)
+
+    console.log("--------------------------------------")
+    const args: any[] = []
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        console.log("Now verifying...")
+        await verify(nft.address, args)
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
